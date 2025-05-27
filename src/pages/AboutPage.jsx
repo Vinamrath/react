259ddf187edc40a6ae7cb1bf74a94c8a4fcff296
Vinamrath <saivinamrath@gmail.com>
2025-05-27 02:50:31 -0500

    fix: Correct typo in Men's Clothing card title

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -18,7 +18,7 @@ const AboutPage = () => {
             <div className="card h-100">
               <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Men's Clothing" height={160} />
               <div className="card-body">
-                <h5 className="card-title text-center">Mens's Clothing</h5>
+                <h5 className="card-title text-center">Men's Clothing</h5>
               </div>
             </div>
           </div>
